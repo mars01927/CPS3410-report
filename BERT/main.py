4458d6b1ef827d3364c88e7f2c0c8d5ccaad2787
Mark <address>
2023-11-27 03:14:38 +0800

    change

--- BERT/main.py
+++ BERT/main.py
@@ -43,17 +43,12 @@ labelencoder = LabelEncoder()
 df['category_1'] = labelencoder.fit_transform(df['category'])
 df[['category', 'category_1']].drop_duplicates(keep='first')
 df.rename(columns={'category_1': 'label'}, inplace=True)
-## create label and sentence list
 sentences = df.clean_text.values
 
-# check distribution of data based on labels
 print("Distribution of data based on labels: ", df.label.value_counts())
 
-# Set the maximum sequence length. The longest sequence in our training set is 47, but we'll leave room on the end anyway.
-# In the original paper, the authors used a length of 512.
 MAX_LEN = 256
 
-## Import BERT tokenizer, that is used to convert our text into tokens that corresponds to BERT library
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
 
 input_ids = [
@@ -65,9 +60,8 @@ labels = df.label.values
 print("Actual sentence before tokenization: ", sentences[2])
 print("Encoded Input from dataset: ", input_ids[2])
 
-## Create attention mask
+
 attention_masks = []
-## Create a mask of 1 for all input tokens and 0 for all padding tokens
 attention_masks = [[float(i > 0) for i in seq] for seq in input_ids]
 print(attention_masks[2])
 
@@ -75,7 +69,6 @@ train_inputs, validation_inputs, train_labels, validation_labels = train_test_sp
                                                                                     test_size=0.1)
 train_masks, validation_masks, _, _ = train_test_split(attention_masks, input_ids, random_state=41, test_size=0.1)
 
-# convert all our data into torch tensors, required data type for our model
 train_inputs = torch.tensor(train_inputs)
 validation_inputs = torch.tensor(validation_inputs)
 train_labels = torch.tensor(train_labels)
@@ -83,11 +76,8 @@ validation_labels = torch.tensor(validation_labels)
 train_masks = torch.tensor(train_masks)
 validation_masks = torch.tensor(validation_masks)
 
-# Select a batch size for training. For fine-tuning BERT on a specific task, the authors recommend a batch size of 16 or 32
 batch_size = 32
 
-# Create an iterator of our data with torch DataLoader. This helps save on memory during training because, unlike a for loop,
-# with an iterator the entire dataset does not need to be loaded into memory
 train_data = TensorDataset(train_inputs, train_masks, train_labels)
 train_sampler = RandomSampler(train_data)
 train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
@@ -98,74 +88,49 @@ validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, 
 
 train_data[0]
 
-# Load BertForSequenceClassification, the pretrained BERT model with a single linear classification layer on top.
 model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=3).to(device)
 
-# Parameters:
 lr = 2e-5
 adam_epsilon = 1e-8
 
-# Number of training epochs (authors recommend between 2 and 4)
 epochs = 3
 
 num_warmup_steps = 0
 num_training_steps = len(train_dataloader) * epochs
 
-### In Transformers, optimizer and schedules are splitted and instantiated like this:
 optimizer = AdamW(model.parameters(), lr=lr, eps=adam_epsilon,
                   correct_bias=False)  # To reproduce BertAdam specific behavior set correct_bias=False
 scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=num_warmup_steps,
                                             num_training_steps=num_training_steps)  # PyTorch scheduler
 
-## Store our loss and accuracy for plotting
 train_loss_set = []
 learning_rate = []
 
-# Gradients gets accumulated by default
 model.zero_grad()
 
-# tnrange is a tqdm wrapper around the normal python range
 for _ in tnrange(1, epochs + 1, desc='Epoch'):
     print("<" + "=" * 22 + F" Epoch {_} " + "=" * 22 + ">")
-    # Calculate total loss for this epoch
     batch_loss = 0
 
     for step, batch in enumerate(train_dataloader):
-        # Set our model to training mode (as opposed to evaluation mode)
         model.train()
 
-        # Add batch to GPU
         batch = tuple(t.to(device) for t in batch)
-        # Unpack the inputs from our dataloader
         b_input_ids, b_input_mask, b_labels = batch
 
-        # Forward pass
         outputs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask, labels=b_labels)
         loss = outputs[0]
 
-        # Backward pass
         loss.backward()
 
-        # Clip the norm of the gradients to 1.0
-        # Gradient clipping is not in AdamW anymore
         torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-
-        # Update parameters and take a step using the computed gradient
         optimizer.step()
-
-        # Update learning rate schedule
         scheduler.step()
-
-        # Clear the previous accumulated gradients
         optimizer.zero_grad()
-
-        # Update tracking variables
         batch_loss += loss.item()
 
-    # Calculate the average loss over the training data.
     avg_train_loss = batch_loss / len(train_dataloader)
 
-    # store the current learning rate
     for param_group in optimizer.param_groups:
         print("\n\tCurrent Learning rate: ", param_group['lr'])
         learning_rate.append(param_group['lr'])
@@ -173,26 +138,18 @@ for _ in tnrange(1, epochs + 1, desc='Epoch'):
     train_loss_set.append(avg_train_loss)
     print(F'\n\tAverage Training loss: {avg_train_loss}')
 
-    # Validation
-
-    # Put model in evaluation mode to evaluate loss on the validation set
     model.eval()
 
-    # Tracking variables
     eval_accuracy, eval_mcc_accuracy, nb_eval_steps = 0, 0, 0
 
-    # Evaluate data for one epoch
     for batch in validation_dataloader:
-        # Add batch to GPU
+
         batch = tuple(t.to(device) for t in batch)
-        # Unpack the inputs from our dataloader
         b_input_ids, b_input_mask, b_labels = batch
-        # Telling the model not to compute or store gradients, saving memory and speeding up validation
         with torch.no_grad():
-            # Forward pass, calculate logit predictions
+
             logits = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
 
-        # Move logits and labels to CPU
         logits = logits[0].to('cpu').numpy()
         label_ids = b_labels.to('cpu').numpy()
 
@@ -218,10 +175,6 @@ for _ in tnrange(1, epochs + 1, desc='Epoch'):
                               normalize=False,
                               title='Confusion matrix',
                               cmap=plt.cm.Blues):
-        """
-        This function prints and plots the confusion matrix.
-        Normalization can be applied by setting `normalize=True`.
-        """
         import itertools
         if normalize:
             cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
@@ -249,7 +202,6 @@ for _ in tnrange(1, epochs + 1, desc='Epoch'):
         plt.xlabel('Predicted label')
         plt.tight_layout()
 
-        ## emotion labels
         label2int = {
             "Negative": -1,
             "Neutral": 0,
@@ -257,3 +209,4 @@ for _ in tnrange(1, epochs + 1, desc='Epoch'):
         }
         print(classification_report(df_metrics['Actual_class'].values, df_metrics['Predicted_class'].values,
                                     target_names=label2int.keys(), digits=len(label2int)))
+
